e7add04d690cd031437f2d9e17a85ee97d8d779e
Ben Akehurst <15856820+benAkehurst@users.noreply.github.com>
2021-06-24 11:39:27 +0100

    update feed to use me query to test

--- mobile/src/screens/feed.js
+++ mobile/src/screens/feed.js
@@ -7,35 +7,30 @@ import NoteFeed from '../components/NoteFeed';
 import Loading from '../components/Loading';
 
 // compose our query
-const GET_NOTES = gql`
-  query notes {
-    notes {
+const GET_ME = gql`
+  query me {
+    me {
       id
-      createdAt
-      content
-      favoriteCount
-      author {
-        username
-        id
-        avatar
-      }
+      username
+      email
     }
   }
 `;
 
-const Feed = props => {
-  const { loading, error, data } = useQuery(GET_NOTES);
+const Feed = (props) => {
+  const { loading, error, data } = useQuery(GET_ME);
 
   // if the data is loading, our app will display a loading indicator
   if (loading) return <Loading />;
   // if there is an error fetching the data, display an error message
   if (error) return <Text>Error loading notes</Text>;
   // if the query is successful and there are notes, return the feed of notes
-  return <NoteFeed notes={data.notes} navigation={props.navigation} />;
+  // return <NoteFeed notes={data.notes} navigation={props.navigation} />;
+  return <Text>{data.me.username}</Text>;
 };
 
 Feed.navigationOptions = {
-  title: 'Feed'
+  title: 'Feed',
 };
 
 export default Feed;
